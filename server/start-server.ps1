a0d22653a70847c0c068f2875e637edeed35c4c1
Jadney Ranes Souza Santos <jadney2014@gmail.com>
2025-10-03 18:03:39 -0300

    Login

--- server/start-server.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "🚀 Iniciando servidor na porta 3001..." -ForegroundColor Green
-cd $PSScriptRoot
-node simple-server.js
--- server/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 🚀 Iniciando servidor de teste...
-cd /d "%~dp0"
-node test-server.js
-pause
